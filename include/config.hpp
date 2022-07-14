045a7ec3b0b6256100914a667ed626f58f03a590
Noemi <noemiruppert@gmail.com>
2022-07-14 20:50:40 +0200

    Olaf pucked on water

--- include/config.hpp
+++ include/config.hpp
@@ -2,5 +2,5 @@
 
 #include <string>
 
-#define SHADER_ROOT std::string("C:/Users/lenna/OneDrive/Documents/GitHub/pengwing/shaders/")
-#define DATA_ROOT std::string("C:/Users/lenna/OneDrive/Documents/GitHub/pengwing/data/")
+#define SHADER_ROOT std::string("C:/Users/noemi/pengwing/shaders/")
+#define DATA_ROOT std::string("C:/Users/noemi/pengwing/data/")
