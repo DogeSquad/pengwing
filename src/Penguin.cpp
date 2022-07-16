a1c2e0e47d88f2acaa66a2529feacb9675bfb6fb
DogeSquad <lennart.stellmach@web.de>
2022-07-16 10:45:06 +0200

    This demo worked

--- src/Penguin.cpp
+++ src/Penguin.cpp
@@ -23,7 +23,7 @@ void setupAnimation(Animator *positionAnimator, Animator *rotationAnimator, Anim
 	// Setup Start
 	positionAnimator->AddKeyframe(0, glm::vec4(0.0f, 0.0f, 0.0f, 1.0f), Animator::HOLD);
 	rotationAnimator->AddKeyframe(0, glm::vec4(0.0f, 1.0f, 0.0f, 0.0f), Animator::HOLD);
-	scaleAnimator->AddKeyframe(0, glm::vec4(glm::vec3(0.5f), 1.0f), Animator::HOLD);
+	scaleAnimator->AddKeyframe(0, glm::vec4(glm::vec3(0.8f), 1.0f), Animator::HOLD);
 
 
 	positionAnimator->AddKeyframe(60, glm::vec4(0.2f, 0.0f, 0.0f, 1.0f), Animator::LINEAR);
@@ -36,7 +36,7 @@ void setupAnimation(Animator *positionAnimator, Animator *rotationAnimator, Anim
 
 	positionAnimator->AddKeyframe(1800, glm::vec4(0.0f, 0.0f, 0.0f, 1.0f), Animator::HOLD);
 	rotationAnimator->AddKeyframe(1800, glm::vec4(0.0f, 1.0f, 0.0f, 0.0f), Animator::HOLD);
-	scaleAnimator->AddKeyframe(1800, glm::vec4(glm::vec3(0.5f), 1.0f), Animator::HOLD);
+	scaleAnimator->AddKeyframe(1800, glm::vec4(glm::vec3(0.8f), 1.0f), Animator::HOLD);
 }
 
 void waddle(unsigned int frameStart, unsigned int frameEnd, float height, glm::vec4 initPos, Animator *positionAnimator, Animator *rotationAnimator)
@@ -46,12 +46,12 @@ void waddle(unsigned int frameStart, unsigned int frameEnd, float height, glm::v
 	if (duration <= 0) return;
 
 	positionAnimator->AddKeyframe(frameStart + quarter_duration, initPos + glm::vec4(0.0f, height, 0.0f, 1.0f), Animator::EASE_OUT_SINE);
-	positionAnimator->AddKeyframe(frameStart + 2 * quarter_duration, initPos + glm::vec4(-0.2f, 0.0f, 0.0f, 1.0f), Animator::EASE_IN_SINE);
+	positionAnimator->AddKeyframe(frameStart + 2 * quarter_duration, initPos + glm::vec4(0.0f, 0.0f, -0.2f, 1.0f), Animator::EASE_IN_SINE);
 	positionAnimator->AddKeyframe(frameStart + 3 * quarter_duration, initPos + glm::vec4(0.0f, height, 0.0f, 1.0f), Animator::EASE_OUT_SINE);
-	positionAnimator->AddKeyframe(frameEnd, initPos + glm::vec4(0.2f, 0.0f, 0.0f, 1.0f), Animator::EASE_IN_SINE);
+	positionAnimator->AddKeyframe(frameEnd, initPos + glm::vec4(0.0f, 0.0f, 0.2f, 1.0f), Animator::EASE_IN_SINE);
 
-	rotationAnimator->AddKeyframe(frameStart + quarter_duration, glm::vec4(0.0f, 0.0f, 1.0f, 0.0f), Animator::EASE_IN);
-	rotationAnimator->AddKeyframe(frameStart + 2 * quarter_duration, glm::vec4(0.0f, 0.0f, 1.0f, 0.1f), Animator::EASE_OUT);
-	rotationAnimator->AddKeyframe(frameStart + 3 * quarter_duration, glm::vec4(0.0f, 0.0f, 1.0f, 0.0f), Animator::EASE_IN);
-	rotationAnimator->AddKeyframe(frameEnd, glm::vec4(0.0f, 0.0f, 1.0f, -0.1f), Animator::EASE_OUT);
+	rotationAnimator->AddKeyframe(frameStart + quarter_duration, glm::vec4(1.0f, 0.0f, 0.0f, 0.0f), Animator::EASE_IN);
+	rotationAnimator->AddKeyframe(frameStart + 2 * quarter_duration, glm::vec4(1.0f, 0.0f, 0.0f, 0.1f), Animator::EASE_OUT);
+	rotationAnimator->AddKeyframe(frameStart + 3 * quarter_duration, glm::vec4(1.0f, 0.0f, 0.0f, 0.0f), Animator::EASE_IN);
+	rotationAnimator->AddKeyframe(frameEnd, glm::vec4(1.0f, 0.0f, 0.0f, -0.1f), Animator::EASE_OUT);
 }
